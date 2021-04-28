4cca16786253e3a8c7cbe3faeb499e073a68e6c0
Patricia <2202416@my.ipleiria.pt>
2021-04-28 19:30:28 +0100

    Adicionar arquivos de projeto.

---
+++ Gestão de Produtos vendidos pela Loja.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bookids
+{
+    public partial class Gestão_de_Produtos_vendidos_pela_Loja : Form
+    {
+        public Gestão_de_Produtos_vendidos_pela_Loja()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gestão de vendas e de Produtos e Gestão de eventos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bookids
+{
+    public partial class Gestão_de_vendas_e_de_Produtos_e_Gestão_de_eventos : Form
+    {
+        public Gestão_de_vendas_e_de_Produtos_e_Gestão_de_eventos()
+        {
+            InitializeComponent();
+        }
+    }
+}
